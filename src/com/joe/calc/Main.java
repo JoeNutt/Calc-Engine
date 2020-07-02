4b1a687d1f79d72625f1a403f96862d1f869f60d
JoeNutt <joenutt01@gmail.com>
2020-07-02 19:24:59 +0100

    Added Loops and Arrays

--- src/com/joe/calc/Main.java
+++ src/com/joe/calc/Main.java
@@ -2,30 +2,34 @@ package com.joe.calc;
 
 public class Main {
 
-    public static void main(String[] args) {
-	double doubleValue1 = 100.0d;
-	double doubleValue2 = 50.0d;
-	char charOpcode = 'd';
-	double doubleResult = 0.0d;
+	public static void main(String[] args) {
+		double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+		double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
+		char[] opCodes = {'d', 'a', 's', 'm'};
+		double[] Results = new double[opCodes.length];
 
-	switch (charOpcode){
-		case 'a':
-        doubleResult = doubleValue1 + doubleValue2;
-        break;
-		case 's':
-        doubleResult = doubleValue1 - doubleValue2;
-        break;
-		case 'm':
-        doubleResult = doubleValue1 * doubleValue2;
-        break;
-		case'd':
-			doubleResult = doubleValue2 != 0 ? doubleValue1 / doubleValue2 : 0.00d;
-			break;
-		default:
-		System.out.println("invalid Opcode"+ charOpcode);
-		doubleResult = 0.0d;
-		break;
+		for (int i = 0; i < opCodes.length; i++) {
+			switch (opCodes[i]) {
+				case 'a':
+					Results[i] = leftVals[i] + rightVals[i];
+					break;
+				case 's':
+					Results[i] = leftVals[i] - rightVals[i];
+					break;
+				case 'm':
+					Results[i] = leftVals[i] * rightVals[i];
+					break;
+				case 'd':
+					Results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.00d;
+					break;
+				default:
+					System.out.println("invalid Opcode" + opCodes[i]);
+					Results[i] = 0.0d;
+					break;
+			}
+			}
+			for(double currentResult : Results)
+			System.out.println(currentResult);
+
 	}
-	System.out.println(doubleResult);
-    }
 }
